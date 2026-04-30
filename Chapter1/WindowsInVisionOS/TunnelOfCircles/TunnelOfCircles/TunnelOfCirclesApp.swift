9efbd5082e7d15f93619e42d96b732a8489e0276
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-01 00:46:58 +0900

    Create Chapter1 WindowsInVisionOS Tutorial (visionOS 튜토리얼 챕터1 추가)

---
+++ Chapter1/WindowsInVisionOS/TunnelOfCircles/TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Shayne Ryu on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
