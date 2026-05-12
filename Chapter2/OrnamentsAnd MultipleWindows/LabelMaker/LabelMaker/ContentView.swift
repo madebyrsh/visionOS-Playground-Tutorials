2c9de4d89ca51ee4932d62b0a6ab5b74b7b29942
SEHWAN YOO <madebyrsh.dev@gmail.com>
2026-05-12 20:20:58 +0900

    Add Slider width [  Slider의 width 너비를 .Frame으로 추가 ]

--- Chapter2/OrnamentsAnd MultipleWindows/LabelMaker/LabelMaker/ContentView.swift
+++ Chapter2/OrnamentsAnd MultipleWindows/LabelMaker/LabelMaker/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
             .ornament(attachmentAnchor: .scene(.bottom)) { // ornament란? attachAnchor이란 .scene(.bottom)이란 그리고 scene의 기준점은?
                 HStack(spacing: 30) {
                     Slider(value: $label.cornerRadius, in: 0...100)
+                        .frame(width: 100)
                 }
                 .padding()
                 .glassBackgroundEffect()
