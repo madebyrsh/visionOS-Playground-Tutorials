e5f9b120c2d8bf4a8657b7dc7c3a6b1182d8d4dc
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-12 09:57:47 +0900

    Create LabeView.swift [ LabelView.swift 생성 ]

---
+++ Chapter2/OrnamentsAnd MultipleWindows/LabelMaker/LabelMaker/LabelView.swift
@@ -0,0 +1,18 @@
+//
+//  LabelView.swift
+//  LabelMaker
+//
+//  Created by Shayne Ryu on 5/12/26.
+//
+
+import SwiftUI
+
+struct LabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabelView()
+}
