8c0bcaf74092c3f3e103908b6093004a86b9311f
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-12 09:46:47 +0900

    Create Chapter2 Ornaments And Multiple Windows Tutorial [ visionOS 튜토리얼 챕터2 추가 ]

---
+++ Chapter2/OrnamentsAnd MultipleWindows/LabelMaker/LabelMaker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LabelMaker
+//
+//  Created by Shayne Ryu on 5/12/26.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Chapter2/OrnamentsAnd MultipleWindows/LabelMaker/LabelMaker/LabelMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelMakerApp.swift
+//  LabelMaker
+//
+//  Created by Shayne Ryu on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LabelMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
