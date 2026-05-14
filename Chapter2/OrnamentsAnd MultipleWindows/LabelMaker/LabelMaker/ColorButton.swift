dfd0e5fb82c53671dfe5e8c8dfe167db006858ba
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-14 10:10:53 +0900

    Add ColorButton.swift [ ColorButton.Swift 생성 ]

---
+++ Chapter2/OrnamentsAnd MultipleWindows/LabelMaker/LabelMaker/ColorButton.swift
@@ -0,0 +1,18 @@
+//
+//  ColorButton.swift
+//  LabelMaker
+//
+//  Created by Shayne Ryu on 5/14/26.
+//
+
+import SwiftUI
+
+struct ColorButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorButton()
+}
