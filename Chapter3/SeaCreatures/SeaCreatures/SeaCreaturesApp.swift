335b1812d42d6dd44f4518b5fdd55405c56871ea
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-16 23:44:38 +0900

    Add defaultSize and windowResizability by contentSize [ 첫 창의 기본창 사이즈 및 컨텐츠 사이즈에 맞게 추가 ]

--- Chapter3/SeaCreatures/SeaCreatures/SeaCreaturesApp.swift
+++ Chapter3/SeaCreatures/SeaCreatures/SeaCreaturesApp.swift
@@ -13,6 +13,8 @@ struct SeaCreaturesApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowResizability(.contentSize)
+        .defaultSize(width: 0.75, height: 0.5, depth: 0.5, in: .meters)
         
         WindowGroup(id: "creatureWindow", for: String.self) { $modelName in // 이부분의 코드를 하나도 모르겠다 어떤 의미인지
             SeaCreatureDetailView(modelName: modelName ?? "ToyBiplane") // 이부분도 이해가 하나도 안된다. modelName: modelName 은 무엇이고 ?? 는 또 무엇이며 왜 갑자기 ToyBiplane을 호출하는지도 모르겠다.
