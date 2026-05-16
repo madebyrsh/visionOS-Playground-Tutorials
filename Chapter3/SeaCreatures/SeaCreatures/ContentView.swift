b55090d10c2a66a84d05970890606deb97904fd7
SEHWAN YOO <madebyrsh.dev@gmail.com>
2026-05-16 17:41:34 +0900

    Add SeaCreatures 3DModel [ 해양생물 3D모델 추가 ]

--- Chapter3/SeaCreatures/SeaCreatures/ContentView.swift
+++ Chapter3/SeaCreatures/SeaCreatures/ContentView.swift
@@ -12,7 +12,7 @@ import RealityKitContent
 struct ContentView: View {
     var body: some View {
         VStack {
-            Model3D(named: "ToyBiplane", bundle: realityKitContentBundle)
+            Model3D(named: "ClamScene", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
             Text("Hello, world!")
