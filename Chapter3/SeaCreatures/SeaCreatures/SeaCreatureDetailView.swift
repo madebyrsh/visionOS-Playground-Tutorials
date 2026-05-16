ad5d6091b86593f5c8d08e529e0943806ae3bdbe
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-16 22:14:04 +0900

    Create SeaCreatureDetailView [ SeaCreatureDetailView 생성 ]

---
+++ Chapter3/SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SeaCreatureDetailView.swift
+//  SeaCreatures
+//
+//  Created by Shayne Ryu on 5/16/26.
+//
+
+import SwiftUI
+
+struct SeaCreatureDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeaCreatureDetailView()
+}
