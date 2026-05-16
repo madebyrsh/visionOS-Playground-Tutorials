d076926ebec9ad2309f261a7a9f6b816cf6ac397
SEHWAN YOO <madebyrsh.dev@gmail.com>
2026-05-16 16:30:06 +0900

    Create Chapter3 Volumes in visionOS Tutorial [ visionOS 튜토리얼 챕터3 추가 ]

---
+++ Chapter3/SeaCreatures/SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Shayne Ryu on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
