121ffbe4cb7dda3f4ee93c4fe21a992b24149b8c
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-16 22:15:37 +0900

    Update #Preview [ #Preview 수정 ]

--- Chapter3/SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
+++ Chapter3/SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct SeaCreatureDetailView: View {
+    
+    let modelName: String // 이 상수를 생성하자마자 #Preview에 Missing argument 에러가 뜨는 이유는?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SeaCreatureDetailView()
+    SeaCreatureDetailView(modelName: "SlugScene")
 }
