ce306de72867d24ea9a63d7ea8ccbea54ac7fef8
SEHWAN YOO <madebyrsh.dev@gmail.com>
2026-05-16 17:17:30 +0900

    Update Model3d ToyBiplane [ Model3d ToyBiplane 으로 변경 ]

--- Chapter3/SeaCreatures/SeaCreatures/ContentView.swift
+++ Chapter3/SeaCreatures/SeaCreatures/ContentView.swift
@@ -12,7 +12,7 @@ import RealityKitContent
 struct ContentView: View {
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
+            Model3D(named: "ToyBiplane", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
             Text("Hello, world!")
