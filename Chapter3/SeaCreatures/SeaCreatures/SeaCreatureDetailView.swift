43be3b3b62b479108380dcd5d04b94db75b40714
madebyrsh <madebyrsh.dev@gmail.com>
2026-05-16 23:40:13 +0900

    Update Onchanged [ + endHorizontalRotation and endVerticalRotation 을 onChanged에 추가 ]

--- Chapter3/SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
+++ Chapter3/SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
@@ -13,8 +13,12 @@ struct SeaCreatureDetailView: View {
     
     let modelName: String // 이 상수를 생성하자마자 #Preview에 Missing argument 에러가 뜨는 이유는?
     
-    @State private var horizontalRotation = CGFloat.zero
+    @State private var horizontalRotation = CGFloat.zero // CGFloat.zero의 zero는 왜있으며 왜 디폴트 수치를 안넣고 CGFloat라고 하였는가?
     @State private var verticalRotation = CGFloat.zero
+    
+    @State private var endHorizontalRotation = CGFloat.zero
+    
+    @State private var endVerticalRotation = CGFloat.zero
     
     var body: some View {
         Model3D(named: modelName, bundle: realityKitContentBundle)
@@ -27,9 +31,13 @@ struct SeaCreatureDetailView: View {
             .gesture(
                 DragGesture()
                     .onChanged({ value in //.onChanged는 무엇인가? 그리고 왜 value in 인가 또한 왜 () 안에 {}를 넣을수 있는가?
-                        horizontalRotation = value.translation.width //여기서 translation.width는 무엇인가
-                        verticalRotation = value.translation.height
+                        horizontalRotation = value.translation.width + endHorizontalRotation //여기서 translation.width는 무엇인가
+                        verticalRotation = value.translation.height + endVerticalRotation // 왜 + endVerticalRotation인가
                         
+                    })
+                    .onEnded ({ _ in // 왜 _를 사용했는가
+                        endHorizontalRotation = horizontalRotation
+                        endVerticalRotation = verticalRotation // 왜 여기서 endVerticalRotation은 value.translation.height같은 형태라 아니라 그냥 verticalRotation 인가?
                     })
                 
                 
